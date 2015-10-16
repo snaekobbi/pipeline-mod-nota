9ce95be9042d39c4065639a0dda7ac71c38647e4
Bert Frees <bertfrees@gmail.com>
2015-10-16 10:20:41 +0200

    Update to mod-braille 1.9.6

--- src/main/java/nu/nota/pipeline/braille/impl/NotaCSSBlockTransform.java
+++ src/main/java/nu/nota/pipeline/braille/impl/NotaCSSBlockTransform.java
@@ -4,27 +4,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.net.URI;
 import javax.xml.namespace.QName;
 
-import static com.google.common.base.Objects.toStringHelper;
-import static com.google.common.collect.Iterables.transform;
+import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
 
 import static org.daisy.pipeline.braille.css.Query.parseQuery;
 import static org.daisy.pipeline.braille.common.util.Tuple3;
 import static org.daisy.pipeline.braille.common.util.URIs.asURI;
+import org.daisy.pipeline.braille.common.AbstractTransform;
+import org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Function;
+import org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Iterables;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Iterables.concat;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Iterables.transform;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.logCreate;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.logSelect;
 import org.daisy.pipeline.braille.common.CSSBlockTransform;
+import org.daisy.pipeline.braille.common.TextTransform;
 import org.daisy.pipeline.braille.common.Transform;
-import org.daisy.pipeline.braille.common.Transform.AbstractTransform;
 import static org.daisy.pipeline.braille.common.Transform.Provider.util.dispatch;
-import static org.daisy.pipeline.braille.common.Transform.Provider.util.logCreate;
-import static org.daisy.pipeline.braille.common.Transform.Provider.util.logSelect;
 import static org.daisy.pipeline.braille.common.Transform.Provider.util.memoize;
 import static org.daisy.pipeline.braille.common.util.Locales.parseLocale;
-import org.daisy.pipeline.braille.common.WithSideEffect;
 import org.daisy.pipeline.braille.common.XProcTransform;
 import org.daisy.pipeline.braille.libhyphen.LibhyphenHyphenator;
 import org.daisy.pipeline.braille.liblouis.LiblouisTranslator;
@@ -36,8 +38,6 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.ComponentContext;
 
-import org.slf4j.Logger;
-
 public interface NotaCSSBlockTransform extends CSSBlockTransform, XProcTransform {
 	
 	@Component(
@@ -47,7 +47,8 @@ public interface NotaCSSBlockTransform extends CSSBlockTransform, XProcTransform
 			CSSBlockTransform.Provider.class
 		}
 	)
-	public class Provider implements XProcTransform.Provider<NotaCSSBlockTransform>, CSSBlockTransform.Provider<NotaCSSBlockTransform> {
+	public class Provider extends AbstractTransform.Provider<NotaCSSBlockTransform>
+		                  implements XProcTransform.Provider<NotaCSSBlockTransform>, CSSBlockTransform.Provider<NotaCSSBlockTransform> {
 		
 		private URI href;
 		
@@ -55,6 +56,12 @@ public interface NotaCSSBlockTransform extends CSSBlockTransform, XProcTransform
 		private void activate(ComponentContext context, final Map<?,?> properties) {
 			href = asURI(context.getBundleContext().getBundle().getEntry("xml/block-translate.xpl"));
 		}
+		
+		private final static String uncontractedTable = "(liblouis-table:'http://www.liblouis.org/tables/da-dk-g16.utb')";
+		private final static String contractedTable = "(liblouis-table:'http://www.liblouis.org/tables/da-dk-g26.ctb')";
+		private final static String hyphenationTable = "(libhyphen-table:'http://www.libreoffice.org/dictionaries/hyphen/hyph_da_DK.dic')";
+		
+		private final static Iterable<NotaCSSBlockTransform> empty = Iterables.<NotaCSSBlockTransform>empty();
 		
 		/**
 		 * Recognized features:
@@ -64,91 +71,65 @@ public interface NotaCSSBlockTransform extends CSSBlockTransform, XProcTransform
 		 * - grade: `1' or `2'.
 		 *
 		 */
-		public Iterable<NotaCSSBlockTransform> get(String query) {
-			 return impl.get(query);
-		 }
-	
-		public Transform.Provider<NotaCSSBlockTransform> withContext(Logger context) {
-			return impl.withContext(context);
-		}
-	
-		private Transform.Provider.MemoizingProvider<NotaCSSBlockTransform> impl = new ProviderImpl(null);
-	
-		private class ProviderImpl extends AbstractProvider<NotaCSSBlockTransform> {
-			
-			private final static String uncontractedTable = "(liblouis-table:'http://www.liblouis.org/tables/da-dk-g16.utb')";
-			private final static String contractedTable = "(liblouis-table:'http://www.liblouis.org/tables/da-dk-g26.ctb')";
-			private final static String hyphenationTable = "(libhyphen-table:'http://www.libreoffice.org/dictionaries/hyphen/hyph_da_DK.dic')";
-		
-			private ProviderImpl(Logger context) {
-				super(context);
-			}
-		
-			protected Transform.Provider.MemoizingProvider<NotaCSSBlockTransform> _withContext(Logger context) {
-				return new ProviderImpl(context);
-			}
-		
-			protected final Iterable<WithSideEffect<NotaCSSBlockTransform,Logger>> __get(String query) {
-				Map<String,Optional<String>> q = new HashMap<String,Optional<String>>(parseQuery(query));
-				Optional<String> o;
-				if ((o = q.remove("locale")) != null)
-					if (!"da".equals(parseLocale(o.get()).getLanguage()))
-						return empty;
-				if ((o = q.remove("translator")) != null)
-					if (o.get().equals("nota"))
-						if ((o = q.remove("grade")) != null) {
-							final int grade;
-							if (o.get().equals("1"))
-								grade = 1;
-							else if (o.get().equals("2"))
-								grade = 2;
-							else
-								return empty;
-							if (q.size() == 0) {
-								Iterable<WithSideEffect<LibhyphenHyphenator,Logger>> hyphenators
-									= logSelect(hyphenationTable, libhyphenHyphenatorProvider.get(hyphenationTable));
-								final String liblouisTable = grade == 1 ? uncontractedTable : contractedTable;
-								return transform(
+		protected final Iterable<NotaCSSBlockTransform> _get(String query) {
+			Map<String,Optional<String>> q = new HashMap<String,Optional<String>>(parseQuery(query));
+			Optional<String> o;
+			if ((o = q.remove("locale")) != null)
+				if (!"da".equals(parseLocale(o.get()).getLanguage()))
+					return empty;
+			if ((o = q.remove("translator")) != null)
+				if (o.get().equals("nota"))
+					if ((o = q.remove("grade")) != null) {
+						final int grade;
+						if (o.get().equals("1"))
+							grade = 1;
+						else if (o.get().equals("2"))
+							grade = 2;
+						else
+							return empty;
+						if (q.size() == 0) {
+							Iterable<LibhyphenHyphenator> hyphenators = logSelect(hyphenationTable, libhyphenHyphenatorProvider);
+							final String liblouisTable = grade == 1 ? uncontractedTable : contractedTable;
+							return concat(
+								transform(
 									hyphenators,
-									new WithSideEffect.Function<LibhyphenHyphenator,NotaCSSBlockTransform,Logger>() {
-										public NotaCSSBlockTransform _apply(LibhyphenHyphenator h) {
-											String translatorQuery = liblouisTable + "(hyphenator:" + h.getIdentifier() + ")";
-											try {
-												applyWithSideEffect(
-													logSelect(
-														translatorQuery,
-														liblouisTranslatorProvider.get(translatorQuery)).iterator().next()); }
-											catch (NoSuchElementException e) {
-												throw new NoSuchElementException(); }
-											return applyWithSideEffect(
-												logCreate(
-													(NotaCSSBlockTransform)new TransformImpl(grade, translatorQuery))); }}); }}
-				return empty;
-			}
+									new Function<LibhyphenHyphenator,Iterable<NotaCSSBlockTransform>>() {
+										public Iterable<NotaCSSBlockTransform> _apply(LibhyphenHyphenator h) {
+											final String translatorQuery = liblouisTable + "(hyphenator:" + h.getIdentifier() + ")";
+											return transform(
+												logSelect(translatorQuery, liblouisTranslatorProvider),
+												new Function<LiblouisTranslator,NotaCSSBlockTransform>() {
+													public NotaCSSBlockTransform _apply(LiblouisTranslator translator) {
+														return __apply(logCreate(new TransformImpl(grade, translatorQuery, translator))); }}); }})); }}
+			return empty;
 		}
-	
-		private final static Iterable<WithSideEffect<NotaCSSBlockTransform,Logger>> empty
-		= Optional.<WithSideEffect<NotaCSSBlockTransform,Logger>>absent().asSet();
 		
 		private class TransformImpl extends AbstractTransform implements NotaCSSBlockTransform {
 			
+			private final LiblouisTranslator translator;
 			private final Tuple3<URI,QName,Map<String,String>> xproc;
 			private final int grade;
 			
-			private TransformImpl(int grade, String translatorQuery) {
+			private TransformImpl(int grade, String translatorQuery, LiblouisTranslator translator) {
 				Map<String,String> options = ImmutableMap.of("query", translatorQuery);
 				xproc = new Tuple3<URI,QName,Map<String,String>>(href, null, options);
 				this.grade = grade;
+				this.translator = translator;
 			}
-	
+			
+			public TextTransform asTextTransform() {
+				return translator;
+			}
+			
 			public Tuple3<URI,QName,Map<String,String>> asXProc() {
 				return xproc;
 			}
 	
 			@Override
 			public String toString() {
-				return toStringHelper(NotaCSSBlockTransform.class.getSimpleName())
+				return Objects.toStringHelper(NotaCSSBlockTransform.class.getSimpleName())
 					.add("grade", grade)
+					.add("id", getIdentifier())
 					.toString();
 			}
 		}
--- src/main/resources/xml/block-translate.xsl
+++ src/main/resources/xml/block-translate.xsl
@@ -11,7 +11,7 @@
 	
 	<xsl:param name="query" required="yes"/>
 	
-	<xsl:template match="css:block" mode="#all">
+	<xsl:template match="css:block" mode="#default before after">
 		<xsl:variable name="text" as="text()*" select="//text()"/>
 		<xsl:variable name="style" as="xs:string*">
 			<xsl:for-each select="$text">
@@ -30,7 +30,7 @@
 		</xsl:apply-templates>
 	</xsl:template>
 	
-	<xsl:template match="css:property[@name=('text-transform','font-style','font-weight','text-decoration','color')]"
+	<xsl:template match="css:property[@name=('font-style','font-weight','text-decoration','color')]"
 	              mode="translate-declaration-list"/>
 	
 	<xsl:template match="css:property[@name='hyphens' and @value='auto']" mode="translate-declaration-list">
